9c0321cd16c5c9d8138711d47d3cc82d9c90373c
ReganLayman <regan.layman@gmail.com>
2016-07-19 18:36:36 -0400

    modified server.js to connect with mongodb database

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/squallispuppeteers');
+
+app.get('/ping', function(req, res) {
+	res.send({ping:'Hello this is server and I am alive!'});
+});
+
+app.get('/ping/:id', function(req, res) {
+	res.send({ping:'hello this is server and I am got '+req.params.id});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
