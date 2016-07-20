f25e07c9524d60012a86c941c1534a4cc0f5544a
ReganLayman <regan.layman@gmail.com>
2016-07-20 17:58:37 -0400

    configure view engine, set view property to path where i'll hold views using app.set and set what the view engine will be using app.set

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ var express = require('express');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'pug');
