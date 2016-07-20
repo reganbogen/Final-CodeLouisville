85525bf24b351a911a58a03fbf0778364e7a622e
ReganLayman <regan.layman@gmail.com>
2016-07-20 18:22:58 -0400

    compile function used by middleware

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var stylus = require('stylus');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'pug');
