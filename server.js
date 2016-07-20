54a8a80c3f8bd3c1061fcc6104ea9b8af9dd9c89
ReganLayman <regan.layman@gmail.com>
2016-07-20 18:14:27 -0400

    create index view in index.pug, link to favicon, add favicon to public folder, tell the application to listen to requests by using app.listen

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'pug');
+
 app.get('*', function(req, res) {
 	res.render('index');
 });
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
