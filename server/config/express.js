0100d6e9ff638cc6993bbdd7121078d19b65b8a7
ReganLayman <regan.layman@gmail.com>
2016-07-21 17:04:03 -0400

    required and installed cookie parser, express session, and passport

--- server/config/express.js
+++ server/config/express.js
@@ -2,6 +2,10 @@ var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+
 
 module.exports = function(app, config) {
 	function compile(str, path) {
@@ -15,6 +19,8 @@ module.exports = function(app, config) {
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(session({secret: 'Squallis Puppeteers Puppets',resave:false,saveUninitialized:false}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.use(stylus.middleware(
 		{
 			src: config.rootPath + '/public',
