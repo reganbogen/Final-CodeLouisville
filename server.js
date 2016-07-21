a640207e32cbb70fd1dc4d125294b13da4984904
ReganLayman <regan.layman@gmail.com>
2016-07-21 01:50:59 -0400

    removed partials and main folder, moved account to app, moved contents of main to main in app

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ db.once('open', function callback() {
 });
 
 app.get('/partials/*', function(req, res) {
-	res.render('partials/' + req.params[0]);
+	res.render('../../public/app/' + req.params[0]);
 })
 
 app.get('*', function(req, res) {
