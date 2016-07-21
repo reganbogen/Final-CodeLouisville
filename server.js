760bb0e035ac04450da8b1f52894b4b4b4514d96
ReganLayman <regan.layman@gmail.com>
2016-07-20 20:11:09 -0400

    created a schema for collection of messages, var messageSchema = mongoose.Schema. created model based on that schema, var Message = mongoose.model. Created variable that will hold data we pull from database, var mongoMessage.  Used this model object to find single doc, no parameter, so it will simply return the first one.  When returned, executed callback function by calling exec function and passing in callback :

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ db.on('error', console.error.bind(console,'connection error...'));
 db.once('open', function callback() {
   console.log('squallispuppeteers db opened');
 });
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
 
 app.get('/partials/:partialPath', function(req, res) {
 	res.render('partials/' + req.params.partialPath);
