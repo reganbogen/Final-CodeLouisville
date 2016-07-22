4514d00fa8dbf9abaf23b58833725e59cb47a9f5
ReganLayman <regan.layman@gmail.com>
2016-07-22 08:47:48 -0400

    final commit for Code Louisville Fullstack JavaScript project, connected to online mongodb database and added uri

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var path =require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
 	development: {
-		db: 'mongodb://localhost/squallispuppeteers',
+		db: 'mongodb://mongodb://rlayman:rlayman@ds027215.mlab.com:27215/squallispuppeteers',
 		rootPath: rootPath,
 		port: process.env.PORT || 3030
 	}
